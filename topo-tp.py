4313d2019c6b7c20f4e36a438c21785bd9a47ff8
Tomas Agata <89418101+tomasagata@users.noreply.github.com>
2022-07-18 17:53:36 -0300

    Update topo-tp.py

--- topo-tp.py
+++ topo-tp.py
@@ -1,3 +1,5 @@
+from mininet.net import Mininet
+from mininet.node import Controller
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
@@ -9,10 +11,10 @@ def topoTP():
     net.addController( 'c0' )
 
     info("*** Adding Hosts\n")
-    emisorFTP = net.addHost('h1' """, ip='172.16.5.2'""")
-    receptorFTP = net.addHost('h2' """, ip='172.16.4.2'""")
-    emisorVideo = net.addHost('h3' """, ip='172.16.3.2'""")
-    receptorVideo = net.addHost('h4' """, ip='172.16.2.2'""")
+    emisorFTP = net.addHost('h1', ip='172.16.5.2')
+    receptorFTP = net.addHost('h2', ip='172.16.4.2')
+    emisorVideo = net.addHost('h3', ip='172.16.3.2')
+    receptorVideo = net.addHost('h4' , ip='172.16.2.2')
     routerA = net.addSwitch('s0')
     routerB = net.addSwitch('s1')
 
